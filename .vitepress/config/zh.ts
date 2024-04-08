1f5ce17943868796a69eb071fc4b8c31222c9e7f
chansee97 <chen.dev@foxmail.com>
2024-04-08 20:24:07 +0800

    fix: modfiy i18n import

--- .vitepress/config/zh.ts
+++ .vitepress/config/zh.ts
@@ -119,7 +119,7 @@ function sidebarGuide(): DefaultTheme.NavItem[] {
       text: "扩展使用",
       items: [
         { text: "使用图标", link: "use-icons" },
-        { text: "国际化", link: "i18n" },
+        { text: "国际化(i18n)", link: "i18n" },
         { text: "UnoCSS", link: "unocss" },
       ],
     },
